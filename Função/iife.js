e0d5fdbe3249cac3a50cf2001ae365040d25e025
G4br1elM4t0s <gabrielmatos010203@gmail.com>
2022-05-12 21:50:58 -0300

    Fugindo do escopo global

---
+++ Função/iife.js
@@ -0,0 +1,8 @@
+//IIFE -> Immediately invoked Function Expression
+
+//foge do escopo global
+(function (){
+    console.log('será executada na hora');
+    console.log('Foge do escopo mais abrangete!');
+
+})()
