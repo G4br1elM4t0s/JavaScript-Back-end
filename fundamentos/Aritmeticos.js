71c493120ff2dff0161f1bb7b27c3007d2d3cd84
Gabriel Matos <gabrielmatos010203@gmail.com>
2022-07-10 20:44:29 -0300

    algumas aterações

--- fundamentos/Aritmeticos.js
+++ fundamentos/Aritmeticos.js
@@ -1,15 +1,13 @@
-const[a,b,c,d] = [3,5,1,15]
+const [a, b, c, d] = [3, 5, 1, 15];
 
 const soma = a + b + c + d;
-const subtracao = a-b;
-const multplica = a*b;
+const subtracao = a - b;
+const multplica = a * b;
 const divisor = a / b;
-const modulo = a%2;
-
+const modulo = a % 2;
 
 //console.log(y)
 
 var y = 2;
 
-
-console.log(y)
+console.log(y);
