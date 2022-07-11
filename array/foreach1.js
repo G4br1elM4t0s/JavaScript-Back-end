3e2aac2fb7f0a9373fbe48552d34c00e7c146693
Gabriel Matos <gabrielmatos010203@gmail.com>
2022-07-11 00:27:54 -0300

    forEach js

---
+++ array/foreach1.js
@@ -0,0 +1,8 @@
+const aprovados = ["Gabriel", "Bia", "Gui", "Lia"];
+
+aprovados.forEach(function(nome, indice){
+    console.log(`${indice + 1} ${nome}`);
+})
+
+
+aprovados.forEach(nome => console.log(nome));
