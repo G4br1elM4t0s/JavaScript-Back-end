cbde1869aef91167c6ee40c99b9b7b39eb9ce619
Gabriel Matos <gabrielmatos010203@gmail.com>
2022-07-28 08:57:58 -0300

    adicionando um banco de dados em memoria

--- node/projeto_1/src/servidor.js
+++ node/projeto_1/src/servidor.js
@@ -4,14 +4,26 @@ const express = require("express");
 //A chamada abaixo está instanciando a chamada express... na variavél app
 // que servirá para colocar o serviços.
 const app =  express();
-
+const bancoDeDados = require('./bancoDeDados');
 
 
 
 
 //teste abaixo
 app.get('/produtos',(req,res,next)=>{
-    res.send({nome:'caneta', preco:1.50}) // Converter para JSON automáticamente pelo metódo send 
+    res.send(bancoDeDados.getProdutos()) // Converter para JSON automáticamente pelo metódo send 
+})
+
+app.get('/produtos/:id',(req,res,next)=>{
+    res.send(bancoDeDados.getProduto(req.params.id)) 
+})
+
+app.post('/produtos', (req,res,next)=>{
+    const produto = bancoDeDados.salvarProduto({
+        nome: req.body.preco,
+        preco: req.body.preco
+    })   
+    res.send(produto) //Final das contas será transformado em JSON
 })
 
 
