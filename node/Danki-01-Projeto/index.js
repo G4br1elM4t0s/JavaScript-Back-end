4d88b02618a0a97d73b887877c27bc26c4d50580
Gabriel Matos <gabrielmatos010203@gmail.com>
2022-08-11 13:57:33 -0300

    Esboço de um projeto demonstração de como é uma aplicação feita com node e armazendo em memoria

---
+++ node/Danki-01-Projeto/index.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const { values } = require('lodash');
+const path = require('path')
+
+const app = express();
+
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'html');
+app.use('/public', express.static(path.join(__dirname, 'public')));
+app.set('views',path.join(__dirname, '/views'));
+
+
+var times= [ 'Vitoria' , 'Bahia', 'Flamengo' , 'Vasco']
+
+app.get('/', (req, res) => {
+    
+    res.render('index',{timesList:times})
+    
+})
+app.get('/deletar/:id', (req, res) => {
+    times = times.filter((val,index)=>{
+        if(index != req.params.id){
+            return values;
+        }
+    })
+    res.render('index',{timesList:times});   
+})
+
+app.listen(5000, ()=>{
+    console.log('Servidor esta rodando!');
+})
+
