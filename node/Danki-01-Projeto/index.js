addee243aa71edd189eab171c9bcbf4e79138ef7
Gabriel Matos <gabrielmatos010203@gmail.com>
2022-08-11 14:24:18 -0300

    finalizando o projetinho de uma lista de times bem basicão

--- node/Danki-01-Projeto/index.js
+++ node/Danki-01-Projeto/index.js
@@ -4,6 +4,13 @@ const path = require('path')
 
 const app = express();
 
+var bodyParser = require('body-parser')
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
 app.engine('html', require('ejs').renderFile);
 app.set('view engine', 'html');
 app.use('/public', express.static(path.join(__dirname, 'public')));
@@ -11,6 +18,12 @@ app.set('views',path.join(__dirname, '/views'));
 
 
 var times= [ 'Vitoria' , 'Bahia', 'Flamengo' , 'Vasco']
+
+app.post('/',(req, res) => {
+    
+    times.push(req.body.time);
+    res.render('index',{timesList:times})
+})
 
 app.get('/', (req, res) => {
     
