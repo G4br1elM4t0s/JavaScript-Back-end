53105781f755aab2a2f4fb121f5dd5264efc8c9c
Gabriel Matos <gabrielmatos010203@gmail.com>
2022-08-13 07:20:47 -0300

    Mais um projeto feito com node- Criando o front de uma aplicação de noticias

--- node/Danki-02-Projeto/index.js
+++ node/Danki-02-Projeto/index.js
@@ -14,7 +14,24 @@ app.use(bodyParser.urlencoded({
 app.engine('html', require('ejs').renderFile);
 app.set('view engine', 'html');
 app.use('/public', express.static(path.join(__dirname, 'public')));
-app.set('views',path.join(__dirname, '/views'));
+app.set('views',path.join(__dirname, '/pages'));
+
+
+app.get('/',(req, res)=>{
+
+    if(req.query.busca == null){
+        res.render('home',{})
+    }
+    else{
+        res.send('Você buscou: ' + req.query.busca)
+    }
+})
+
+app.get('/:slug',(req, res)=>{
+    
+    res.send(req.params.slug);
+
+})
 
 app.listen(5100, ()=>{
     console.log('Servidor esta rodando!');
